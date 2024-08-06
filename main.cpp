2d398021aedf6a8697f2c0191e51bfa7757fcb49
Pramesh <shakyapramesh@knights.ucf.edu>
2024-08-06 16:13:18 -0400

    CMake working now

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include "dynamic/dynamic.hpp"
+
+using namespace dyn;
 
 int main(){
     std::cout << "Hello world!\n" ;
+    packed_spsi my_spsi;
+    my_spsi.push_back(10);
+    my_spsi.push_back(20);
+    my_spsi.push_back(30);
+
+    std::cout << my_spsi.at(2) << "\n";
     return (EXIT_SUCCESS);
 }
