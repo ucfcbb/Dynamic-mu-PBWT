32af056c454ae6492c0914d39b73fa260a7b9022
Pramesh <shakyapramesh@knights.ucf.edu>
2024-08-07 09:56:40 -0400

    Update cmake and add include dir and file

--- main.cpp
+++ main.cpp
@@ -1,9 +1,58 @@
 #include <iostream>
-#include "dynamic/dynamic.hpp"
+#include "dcpbwt.h"
 
-using namespace dyn;
 
-int main(){
+void PrintHelp() {
+    std::cout << "Usage: dcpbwt [options]\n" << std::endl;
+    std::cout << "Options:" << std::endl;
+    std::cout << "  -i, --input_file <path>\t vcf file for panel" << std::endl;
+    std::cout << "  -v, --verbose <path>\t show detail information" << std::endl;
+    std::cout << "  -h, --help\t show this help message and exit" << std::endl;
+}
+
+int main(int argc, char** argv){
+    if (argc == 1) {
+        PrintHelp();
+        exit(EXIT_SUCCESS);
+    }
+    std::string ref_vcf_input = "";
+    std::string output = "";
+    bool verbose = false;
+
+    int c;
+    while (1){
+        static struct option long_options[] = {
+                {"input",   required_argument, nullptr, 'i'},
+                {"verbose",   no_argument, nullptr, 'v'},
+                {"help",    no_argument,       nullptr, 'h'},
+                {nullptr, 0,                   nullptr, 0}};
+
+        int option_index = 0;
+        c = getopt_long(argc, argv, "i:vh", long_options,
+                        &option_index);
+
+        if (c == -1) {
+                break;
+        }
+
+        switch (c) {
+            case 'i':
+                ref_vcf_input = optarg;
+                break;
+            case 'v':
+                verbose = true;
+                break;
+            case 'h':
+                PrintHelp();
+                exit(EXIT_SUCCESS);
+            default:
+                PrintHelp();
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    DCPBWT dcpbwt(ref_vcf_input.c_str());
+    ReadVCF(ref_vcf_input.c_str());
     std::cout << "Hello world!\n" ;
     packed_spsi my_spsi;
     my_spsi.push_back(10);
